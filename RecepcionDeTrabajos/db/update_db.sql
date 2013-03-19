2353e7f45a9f7455ab26e93a01a5b8495bfeb89a
gabrieltursi@gmail.com <gabrieltursi@gmail.com@1b19ac39-1e74-49a1-8bd1-46ce7e2364f7>
2013-03-19 19:51:29 +0000

    Actualizacion automatica de la base de datos

--- RecepcionDeTrabajos/db/update_db.sql
+++ RecepcionDeTrabajos/db/update_db.sql
@@ -1,3 +1,5 @@
+--AGREGAR AQUI NUEVA VERSIONES! ACTUALIZAR PROPERTY DB_VERSION
 -- Version 20130319
 alter table pedido_item add costo number(18,2);
 alter table pedido_item add precio number(18,2);
+update property set value='20130319' where name='db_version'
