0d327d61b322ad73b7db4b1719b2a4f5856848db
gtursi <gabrieltursi@gmail.com>
2014-09-22 16:36:41 -0300

    Pausar siempre

--- RecepcionDeTrabajos/db/stop_db.bat
+++ RecepcionDeTrabajos/db/stop_db.bat
@@ -1,3 +1,3 @@
 cd %~dp0
-@java -cp h2*.jar org.h2.tools.Server -tcpShutdown tcp://localhost:9092
-@if errorlevel 1 pause
+@java -cp h2.jar org.h2.tools.Server -tcpShutdown tcp://localhost:9092
+pause
